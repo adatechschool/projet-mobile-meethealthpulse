850855cadec3224da2ea61e13b099db4c6e23bd2
HanaaBelle <hanaabelhamidi@gmail.com>
2023-08-28 16:13:40 +0200

    Added initial MHP Xcode project

---
+++ meet_health_pulse/meet_health_pulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  meet_health_pulseApp.swift
+//  meet_health_pulse
+//
+//  Created by Hanâa Belhamidi on 28/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct meet_health_pulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ meet_health_pulseUITests/meet_health_pulseUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  meet_health_pulseUITestsLaunchTests.swift
+//  meet_health_pulseUITests
+//
+//  Created by Hanâa Belhamidi on 28/08/2023.
+//
+
+import XCTest
+
+final class meet_health_pulseUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
