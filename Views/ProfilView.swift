6a893a1890d4146f4624e0e84e3d947775e12d72
HanaaBelle <hanaabelhamidi@gmail.com>
2023-08-31 15:54:56 +0200

    Added ProfilView file with (My Profil) title

---
+++ Views/ProfilView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfilView.swift
+//  meet_health_pulse
+//
+//  Created by Hanâa Belhamidi on 31/08/2023.
+//
+
+import SwiftUI
+
+struct ProfilView: View {
+    var body: some View {
+        Text("My Profil")
+    }
+}
+
+struct ProfilView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfilView()
+    }
+}
