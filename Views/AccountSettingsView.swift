63848665101dccb3ece452c646396262d3992b8d
HanaaBelle <hanaabelhamidi@gmail.com>
2023-08-31 15:18:32 +0200

    Added AccountSettingsView file with (Account Settings) title

---
+++ Views/AccountSettingsView.swift
@@ -0,0 +1,24 @@
+//
+//  AccountSettingsView.swift
+//  meet_health_pulse
+//
+//  Created by Hanâa Belhamidi on 31/08/2023.
+//
+
+import SwiftUI
+
+struct AccountSettingsView: View {
+    var body: some View {
+        NavigationView{
+            ZStack{
+                Text("Account Settings")
+            }
+        }
+    }
+}
+
+struct AccountSettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountSettingsView()
+    }
+}
