4369190da303b490bce98d2c934a27c09adbf68d
HanaaBelle <hanaabelhamidi@gmail.com>
2023-08-31 14:44:19 +0200

    Added menuView file with Navigation links

---
+++ Views/menuView.swift
@@ -0,0 +1,62 @@
+//
+//  menuView.swift
+//  meet_health_pulse
+//
+//  Created by Hanâa Belhamidi on 31/08/2023.
+//
+
+import SwiftUI
+
+struct menuView: View {
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color.white
+                    .cornerRadius(8)
+                VStack {
+                    HStack {
+                        
+                    
+                    }
+                    Spacer()
+                    List {
+                        NavigationLink(destination: Text("Account Settings")) {
+                            HStack {
+                                
+                                Text("Account Settings")
+                                    .multilineTextAlignment(.center)
+                            }
+                        }
+                        NavigationLink(destination: Text("Notifications")) {
+                            HStack {
+                                
+                                Text("Notifications")
+                                    .multilineTextAlignment(.center)
+                            }
+                        }
+                        NavigationLink(destination: Text("Find Friends")) {
+                            HStack {
+                                
+                                Text("Find Friends")
+                                    .multilineTextAlignment(.center)
+                            }
+                        }
+                        NavigationLink(destination: Text("HomeView")) {
+                            HStack {
+                            
+                            Text("Sign Out")
+                                .multilineTextAlignment(.center)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct menuView_Previews: PreviewProvider {
+    static var previews: some View {
+        menuView()
+    }
+}
