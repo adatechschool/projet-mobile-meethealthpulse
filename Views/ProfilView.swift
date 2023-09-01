0d79443675240a8d700949197adacdfa098c2dce
HanaaBelle <hanaabelhamidi@gmail.com>
2023-09-01 13:04:37 +0200

    Added sign out button feature

--- Views/ProfilView.swift
+++ Views/ProfilView.swift
@@ -8,12 +8,15 @@
 import SwiftUI
 
 struct ProfilView: View {
+    
+    // Déclarer une var qui est un état "a state" pour suivre si le bouton "Sign Out" a été appuyé
+    @State private var showHomeView = false
+    
     var body: some View {
         NavigationView{
             ZStack{
                 VStack {
                     HStack {
-                        //Spacer() // ça permet de pousser le contenu vers la gauche laissant l'icône à droite
                         
                         // Lien vers "Menu"
                         NavigationLink(destination: MenuView()) {
@@ -21,36 +24,58 @@ struct ProfilView: View {
                                 .foregroundColor(Color.black)
                                 .imageScale(.large)
                         }
-                        //.padding(.trailing)// Ajout du padding à droite
-                        
                         
                         // Ajout du padding à gauche
                         .padding(.leading)
+                        
                         // ça permet de pousser le contenu vers la droite laissant l'icône à gauche
                         Spacer()
                     }
                     Spacer()
                 }
-                .padding()
-                VStack{
-                    // Ajout de la photo de profil
+                
+                VStack {
+                    
+                    // Affichage de la photo de profil
                     Image("imagepardefault")
                         .resizable()
                         .scaledToFit()
                         .frame(width: 300, height: 300)
-                    // Ajout de l'icon du Sport
+                    
+                    // Ajout de l'icon du Sport ou activité préféré(e)
                     Image(systemName: "figure.open.water.swim")
                         .foregroundColor(Color.black)
                         .padding(.leading)
                         .imageScale(.large)
+                    
                     // Ajout du Welcome (+ Pseudo du profil)
                     Text("Welcome 🥳")
                         .font(.title)
                         .fontWeight(.black)
+                    
+                    // ça permet de pousser le contenu vers le bas d'une hauteur de 200
+                    Spacer()
+                       .frame(height: 200)
+                            
+                            // Sign Out Button pour revenir à la HomeView
+                            Button(action: {
+                                showHomeView.toggle()
+                            }
+                            ) {
+                                HStack {
+                                    Image(systemName: "rectangle.lefthalf.inset.filled.arrow.left")
+                                        .imageScale(.medium)
+                                        .foregroundColor(Color.white)
+                                    Text("Sign Out")
+                                        .foregroundColor(Color.white)
+                                }
+                            }
+                            .buttonStyle(.borderedProminent)
+                            .tint(Color.black)
+                            .padding()
                 }
-                
             }
-            // Définir le titre de navigation
+            // Définir le titre de navigation "My Profil"
             .listStyle(PlainListStyle())
             .navigationTitle("My Profil")
         }
