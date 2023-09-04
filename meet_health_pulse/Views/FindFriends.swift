be8dba79be1bfad4a375ccd7009462b4af5b646e
MischaelP <phemius.p@gmail.com>
2023-09-04 22:55:55 +0200

    View Friendfinder

---
+++ meet_health_pulse/Views/FindFriends.swift
@@ -0,0 +1,20 @@
+//
+//  FindFriends.swift
+//  meet_health_pulse
+//
+//  Created by Mischael Phemius on 04/09/2023.
+//
+
+import SwiftUI
+
+struct FindFriends: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FindFriends_Previews: PreviewProvider {
+    static var previews: some View {
+        FindFriends()
+    }
+}
