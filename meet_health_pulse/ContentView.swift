ae84a94bd0f682e46609e4b3e13a8bf73adbbe8d
MischaelP <phemius.p@gmail.com>
2023-09-05 12:01:06 +0200

    Fixed link

--- meet_health_pulse/ContentView.swift
+++ meet_health_pulse/ContentView.swift
@@ -8,19 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world! MHP de bondy")
-        }
-        .padding()
+        
+        Text("Hello MHP")
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
--- meet_health_pulse/Views/FindFriends.swift
+++ meet_health_pulse/Views/FindFriends.swift
@@ -8,13 +8,51 @@
 import SwiftUI
 
 struct FindFriends: View {
+    @State private var activity = ""
+    @State private var location = ""
+    @State private var minDistance = ""
+    @State private var maxDistance = ""
+    @State private var minAge = ""
+    @State private var maxAge = ""
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct FindFriends_Previews: PreviewProvider {
-    static var previews: some View {
-        FindFriends()
+        
+            Form {
+                Section (header: Text ("Filtres de recherches")) {
+                    TextField("Activité", text: $activity)
+                    TextField("Location", text: $location)
+                    
+                    HStack {
+                        TextField("Distance minimale (km)", text: $minDistance)
+                        
+                    }
+                    HStack {
+                        TextField("Distance maximale (km)", text: $maxDistance)
+                        
+                    }
+                }
+                
+                Section(header: Text("Filtres d'âge")) {
+                    HStack {
+                        TextField("Age minimum", text: $minAge)
+                        Text("ans")
+                    }
+                    HStack {
+                        TextField("Age maximum", text: $maxAge)
+                        Text("ans")
+                    }
+                }
+                Button("Rechercher"){
+                }
+            }
+        }
+        
+    
+    
+    struct FindFriends_Previews: PreviewProvider {
+        static var previews: some View {
+            FindFriends()
+        }
     }
 }
