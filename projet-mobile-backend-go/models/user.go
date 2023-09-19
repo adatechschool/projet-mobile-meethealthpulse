8b9782f1b039efa4e03d35fe548672fa833537f6
HanaaBelle <hanaabelhamidi@gmail.com>
2023-09-19 12:17:56 +0200

    Added user model

--- projet-mobile-backend-go/models/user.go
+++ projet-mobile-backend-go/models/user.go
@@ -3,4 +3,9 @@ package models
 // Définir la structure "User" :
 
 type User struct {
+	ID          string `json:"id"`
+	Username    string `json:"username" bson:"username"`
+	DateOfBirth string `json:"dateOfBirth" bson:"dateOfBirth"`
+	Email       string `json:"email" bson:"email"`
+	Password    string `json:"password" bson:"password"`
 }
