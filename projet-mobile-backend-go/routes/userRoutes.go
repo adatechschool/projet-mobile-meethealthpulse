a7bd2353fc0ed660db2061a131711489d59a9869
HanaaBelle <hanaabelhamidi@gmail.com>
2023-09-19 12:21:20 +0200

    Added userRoutes with signUp&logIn endpoints

--- projet-mobile-backend-go/routes/userRoutes.go
+++ projet-mobile-backend-go/routes/userRoutes.go
@@ -3,6 +3,8 @@ package routes
 
 import (
 
+	// Importer le package models qui définit la structure des utilisateurs
+	"projet-mobile-backend-go/controllers"
 	// Importer le package gin, un cadre très populaire pour écrire des applications en Go
 	"github.com/gin-gonic/gin"
 )
@@ -10,4 +12,22 @@ import (
 // RegisterRoutes prend en argument une référence à un objet "gin.Engine" (le moteur principal de l'application)
 func RegisterRoutes() *gin.Engine {
 
+	route := gin.Default()
+
+	// Endpoint pour l'inscription (signup)
+	// Elle définit une route POST pour l'URL /signup qui, lorsqu'elle est appelée, exécutera la fonction signupHandler
+	route.POST("/signup", controllers.SignupHandler)
+
+	// Endpoint pour la connexion (login)
+	route.POST("/login", controllers.LoginHandler)
+
+	// Endpoint pour la déconnexion (logout)
+	//route.POST("/logout", controllers.LogoutHandler)
+
+	// Ajout du nouvel endpoint pour la racine "/"
+	route.GET("/", func(c *gin.Context) {
+		c.String(200, "Bienvenue sur mon API!")
+	})
+
+	return route
 }
