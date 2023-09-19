f3484914f65a9613ab8df6c33b104e1686fae5a1
HanaaBelle <hanaabelhamidi@gmail.com>
2023-09-19 12:19:32 +0200

    Added config file

--- projet-mobile-backend-go/config/config.go
+++ projet-mobile-backend-go/config/config.go
@@ -1,1 +1,19 @@
 package config
+
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+func LoadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func GetDBCredentials() (string, string) {
+	// pour ajouter une éventuelle : apiKey := os.Getenv("API_KEY")
+	return os.Getenv("DB_USER"), os.Getenv("DB_PASS")
+}
